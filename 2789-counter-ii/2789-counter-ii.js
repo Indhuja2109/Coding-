e443e8f64e7a0a654332248dd4cf818771c6c951
INDHUJA V <140780667+Indhuja2109@users.noreply.github.com>
2024-11-29 17:58:44 +0530

    Time: 56 ms (73.81%), Space: 51.1 MB (93.57%) - LeetHub

--- 2789-counter-ii/2789-counter-ii.js
+++ 2789-counter-ii/2789-counter-ii.js
@@ -1,19 +1,8 @@
-/**
- * @param {integer} init
- * @return { increment: Function, decrement: Function, reset: Function }
- */
 var createCounter = function(init) {
-    let present = init;
+    let presentCount = init
     return {
-        increment:()=> ++present,
-        decrement:()=> --present,
-        reset:()=> present = init,
+        increment:()=> ++presentCount,
+        decrement:()=> --presentCount,
+        reset:()=> presentCount = init,
     }
 };
-
-/**
- * const counter = createCounter(5)
- * counter.increment(); // 6
- * counter.reset(); // 5
- * counter.decrement(); // 4
- */
