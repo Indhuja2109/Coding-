cabb692e6ede32df58f38e46bfc9a9e226b7d818
INDHUJA V <140780667+Indhuja2109@users.noreply.github.com>
2024-12-04 17:26:35 +0530

    Time: 6 ms (71.31%), Space: 45.6 MB (53.29%) - LeetHub

--- 3018-make-string-a-subsequence-using-cyclic-increments/3018-make-string-a-subsequence-using-cyclic-increments.java
+++ 3018-make-string-a-subsequence-using-cyclic-increments/3018-make-string-a-subsequence-using-cyclic-increments.java
@@ -1,12 +1,22 @@
 class Solution {
     public boolean canMakeSubsequence(String source, String target) {
-        int targetIdx = 0, targetLen = target.length();
+        int i = 0, j = 0;
+        int n = source.length(), m = target.length();
         
-        for (char currChar : source.toCharArray()) {
-            if (targetIdx < targetLen && (target.charAt(targetIdx) - currChar + 26) % 26 <= 1) {
-                targetIdx++;
+        while (i < n && j < m) {
+            // Current character in str1
+            char currentChar = source.charAt(i);
+            // Next character cyclically (handling 'z' to 'a')
+            char nextChar = (currentChar == 'z') ? 'a' : (char) (currentChar + 1);
+            
+            // Check if the character matches str2[j] directly or after increment
+            if (currentChar == target.charAt(j) || nextChar == target.charAt(j)) {
+                j++;
             }
+            i++;
         }
-        return targetIdx == targetLen;
+        
+        // If we traversed all characters in str2, return true
+        return j == m;
     }
 }
